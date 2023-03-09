3c6a276438bde7aed261cc5edafa4f794af527d3
luisfernandezpetra <luisfernandezpetra@gmail.com>
2023-03-08 23:51:50 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,46 @@
+let c1=document.getElementById("c1");
+
+c1.onclick = function(){
+    document.getElementById("c9").innerHTML="Nombre y Apellido:  ";
+    document.getElementById("c10").innerHTML=" Luis Fernandez";
+}
+c2.onclick = function(){
+    document.getElementById("c9").innerHTML="Telefono:  ";
+    document.getElementById("c10").innerHTML=" 054 3537 608413";
+
+}
+
+c3.onclick = function(){
+    document.getElementById("c9").innerHTML="E-mail:  ";
+    document.getElementById("c10").innerHTML=" luisfernandezpetra@gmail.com";
+    }
+
+
+    c4.onclick = function(){
+        document.getElementById("c9").innerHTML="Domicilio:  ";
+        document.getElementById("c10").innerHTML=" Velez Sarsfield 159 - Bell Ville Cordoba Argentina";
+        }
+
+
+        c5.onclick = function(){
+            document.getElementById("c9").innerHTML="Fecha de Nacimiento:  ";
+            document.getElementById("c10").innerHTML=" 27 de Abril de 1967";
+            }
+
+            c6.onclick = function(){
+                document.getElementById("c9").innerHTML="Estudios:  ";
+                document.getElementById("c10").innerHTML=" Bachiller - Programador Web";
+                
+                }
+
+                c7.onclick = function(){
+                    document.getElementById("c9").innerHTML="Trabajos:  ";
+                    document.getElementById("c10").innerHTML=" Banco sudecor Litoral - SISCARD SA";
+                    }
+
+
+
+
+
+
+
